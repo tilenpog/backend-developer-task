541e6560be64804cbed040fb2afbad37d4120801
tilenpog <pugacnik@gmail.com>
2023-04-18 20:58:59 +0200

    Initial project setup

---
+++ src/server.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const app = require("./app");
+
+init();
+
+async function init() {
+  try {
+    const listener = app.listen(process.env.PORT || 3000, () => {
+      console.log(`Listening on port ${listener.address().port}!`);
+    });
+  } catch (error) {
+    console.error(`An error occurred: ${JSON.stringify(error)}`);
+    process.exit(1);
+  }
+}
